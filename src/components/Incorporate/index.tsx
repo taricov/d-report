523b57b21e91a8463fe423a1fba66a4da06a292b
TaricOv <tareq.ouf@gmail.com>
2023-11-08 10:25:50 +0200

    fixed conditions chnge input type

--- src/components/Incorporate/index.tsx
+++ src/components/Incorporate/index.tsx
@@ -201,7 +201,7 @@ useEffect(() => {
       console.log(reportVariables)
       console.log(conditions)
 
-},[reportVariables, tablesVariables])
+},[reportVariables, tablesVariables, conditions])
   return (
     <>
 <div className="w-[100%] m-auto flex justify-center items-center gap-4">
@@ -353,7 +353,7 @@ useEffect(() => {
 </div>
 <div>
 <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Input Type</label>
-<select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+<select id="countries" onChange={(e:ChangeEvent<HTMLSelectElement>)=>setConditions((prev:any)=>conditions.map((con:any, ci)=>ci === i ? {...prev, input_type: e.target.value}: {...con}))} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
   <option selected>Choose Input Type</option>
   <option value="text">Text</option>
   <option value="number">Number</option>
@@ -362,7 +362,7 @@ useEffect(() => {
 </div>
 <div>
             <label htmlFor="expreassion_value" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Value</label>
-            <input type={c.input_type} id="expreassion_value" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required/>
+            <input type={c.input_type} onChange={()=>""} id="expreassion_value" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="New Value" required/>
         </div>
 </li>
 ))
