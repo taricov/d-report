a88fc2e84fefbd31f0a80761f276e6c9a01de97e
TaricOv <tareq.ouf@gmail.com>
2023-11-09 04:22:22 +0200

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import { Hero } from "./components/Hero";
 import Incorporate from "./components/Incorporate";
 
 import {
@@ -8,6 +7,11 @@ import {
   QueryClientProvider
 
 } from 'react-query'
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Reports from "./pages/Reports";
+import Report from "./pages/Report";
 
 const queryClient = new QueryClient()
 
@@ -17,19 +21,18 @@ function App() {
     <>
         <QueryClientProvider client={queryClient}>
           <Header />
-          <Hero/>
-      <div className="flex flex-col items-center bg-gray-100 min-h-screen">
-        <h1 className="text-3xl font-bold mb-14 mt-40 ">
-          D-Report 
-        </h1>
-        <div className="max-w-[50%] text-center px-4 py-6 bg-slate-500 rounded-md text-3xl font-bold mt-12 mb-14 capitalize">
-         this tool is a report builder for Daftra ERP users, with which they can build custom reports in matter of seconds.
-        </div>
-        <Incorporate />
-      </div>
+          <BrowserRouter>
+<Routes>
+<Route  path='/' element={<Home/>} />
+<Route  path='/about' element={<About/>} />
+<Route  path='/reports' element={<Reports/>} />
+<Route  path='/reports/:id' element={<Report/>} />
+
+</Routes>
+
+</BrowserRouter>
     <Footer/>
     </QueryClientProvider>
-
     </>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,20 @@
+import { Hero } from "../../components/Hero";
+import Incorporate from "../../components/Incorporate";
+
 const Home = () => {
     return (
   
       <>
-      
+          <Hero/>
+          <div className="flex flex-col items-center bg-gray-100 min-h-screen">
+        <h1 className="text-3xl font-bold mb-14 mt-40 ">
+          D-Report 
+        </h1>
+        <div className="max-w-[50%] text-center px-4 py-6 bg-slate-500 rounded-md text-3xl font-bold mt-12 mb-14 capitalize">
+         this tool is a report builder for Daftra ERP users, with which they can build custom reports in matter of seconds.
+        </div>
+        <Incorporate />
+      </div>
       </>
     );
   };
